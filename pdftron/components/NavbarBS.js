229c793d390b5dca5b7e3396bdd1d8bad8df5721
Hugo <hugowkwan@gmail.com>
2021-11-04 00:03:43 -0700

    Creates admin dashboard page and moves admin pages there

--- pdftron/components/NavbarBS.js
+++ pdftron/components/NavbarBS.js
@@ -5,12 +5,18 @@ export const NavbarBS = ({isLoggedin}) => {
     // should pass in user object ^ as a prop
     // isLoggedin is just a T/F value rn to disable some links on index.js
 
+    //default values
+    let name = "Profile" 
+    let isAdmin = false
     // if (user) {
-    //     // checks if user is logged in
+    //     // checks if user is logged in and their role
     //     let name = user.name
-    //     let adminPriv = user.admin
+    //     let isAdmin = user.admin
     // }
-    let name = "Williard"
+
+    // placeholder tests
+    name = "Williard"
+    isAdmin = true
 
     return (
 
@@ -27,10 +33,10 @@ export const NavbarBS = ({isLoggedin}) => {
             <Nav>
                 {isLoggedin && <Nav.Link href="/book">Book</Nav.Link> }
                 {isLoggedin && <Nav.Link href="/mybookings">My Bookings</Nav.Link>}
-                {isLoggedin && <Nav.Link href="/usersettings">User Settings</Nav.Link>}
-                {isLoggedin && <Nav.Link href="/allbookings">See All Bookings</Nav.Link>}
+                {/* {isLoggedin && <Nav.Link href="/usersettings">User Settings</Nav.Link>}
+                {isLoggedin && <Nav.Link href="/allbookings">See All Bookings</Nav.Link>} */}
+                {isLoggedin && isAdmin && <Nav.Link href="/admindashboard">Admin Dashboard</Nav.Link>}
                 {isLoggedin && <NavDropdown title={name} id="basic-nav-dropdown">
-                    {/* <NavDropdown.Item href="/edit">Admin Dashboard</NavDropdown.Item> */}
                     <NavDropdown.Item href="#">Settings</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#">Logout</NavDropdown.Item>
@@ -39,6 +45,5 @@ export const NavbarBS = ({isLoggedin}) => {
         </Navbar.Collapse> }
         </Container>
         </Navbar>
-
     );
 };
