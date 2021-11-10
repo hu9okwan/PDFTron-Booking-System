cde239b98b6209c1ae54e6a5561ae1e86e6adc0c
Anson Lit <ansonlit@live.ca>
2021-11-10 09:02:43 -0800

    Remove unecessary Auth URL for Google Provider

--- pdftron/pages/api/auth/[...nextauth].js
+++ pdftron/pages/api/auth/[...nextauth].js
@@ -7,18 +7,6 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code'
     })
-  ],
-  callbacks: {
-    async signIn(user, account, profile) {
-      if (account.provider === 'google' &&
-          profile.verified_email === true &&
-          profile.email.endsWith('@pdftron.com')) {
-        return true
-      } else {
-        return false
-      }
-    },
-  }
+  ]
 })
