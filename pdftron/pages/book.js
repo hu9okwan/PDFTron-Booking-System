ab41a5df53df5115d14e4cd78ac25eb0baec4a7a
Hugo <hugowkwan@gmail.com>
2021-11-24 02:52:51 -0800

    Canvas can be panned like google maps style

--- pdftron/pages/book.js
+++ pdftron/pages/book.js
@@ -99,8 +99,8 @@ import TableDatePicker from "../components/datepicker";
                 canvas.hoverCursor = 'pointer';
                 clickTable(canvas, teamObj);
                 hoverTable(canvas, teamObj);
+                panningZoom(canvas)
             })
-
         }
     }, [canvas]);
 
@@ -235,6 +235,79 @@ import TableDatePicker from "../components/datepicker";
         }
     }
 
+    const panningZoom = (canvas) => {
+
+        // canvas.on('mouse:wheel', function(opt) {
+        //     var delta = opt.e.deltaY;
+        //     var zoom = canvas.getZoom();
+        //     zoom *= 0.999 ** delta;
+        //     if (zoom > 20) zoom = 20;
+        //     if (zoom < 0.01) zoom = 0.01;
+        //     canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+        //     opt.e.preventDefault();
+        //     opt.e.stopPropagation();
+        //     var vpt = this.viewportTransform;
+        //     if (zoom < 0.4) {
+        //       vpt[4] = 200 - 800 * zoom / 2;
+        //       vpt[5] = 200 - 800 * zoom / 2;
+        //     } else {
+        //       if (vpt[4] >= 0) {
+        //         vpt[4] = 0;
+        //       } else if (vpt[4] < canvas.getWidth() - 800 * zoom) {
+        //         vpt[4] = canvas.getWidth() - 800 * zoom;
+        //       }
+        //       if (vpt[5] >= 0) {
+        //         vpt[5] = 0;
+        //       } else if (vpt[5] < canvas.getHeight() - 800 * zoom) {
+        //         vpt[5] = canvas.getHeight() - 800 * zoom;
+        //       }
+        //     }
+        //   });
+          canvas.on('mouse:down', function(opt) {
+            var evt = opt.e;
+            if (!opt.target) {
+                canvas.setCursor("grabbing")
+              this.isDragging = true;
+              this.selection = false;
+              this.lastPosX = evt.clientX;
+              this.lastPosY = evt.clientY;
+            }
+          });
+          canvas.on('mouse:move', function(opt) {
+            if (this.isDragging) {
+              var e = opt.e;
+              var zoom = canvas.getZoom();
+              var vpt = this.viewportTransform;
+              if (zoom < 0.4) {
+                vpt[4] = 200 - 800 * zoom / 2;
+                vpt[5] = 200 - 800 * zoom / 2;
+              } else {
+                vpt[4] += e.clientX - this.lastPosX;
+                vpt[5] += e.clientY - this.lastPosY;
+                if (vpt[4] >= 0) {
+                  vpt[4] = 0;
+                } else if (vpt[4] < canvas.getWidth() - 1000 * zoom) {
+                  vpt[4] = canvas.getWidth() - 1000 * zoom;
+                }
+                if (vpt[5] >= 0) {
+                  vpt[5] = 0;
+                } else if (vpt[5] < canvas.getHeight() - 800 * zoom) {
+                  vpt[5] = canvas.getHeight() - 800 * zoom;
+                }
+              }
+              this.requestRenderAll();
+              this.lastPosX = e.clientX;
+              this.lastPosY = e.clientY;
+            }
+          });
+          canvas.on('mouse:up', function(opt) {
+            this.setViewportTransform(this.viewportTransform);
+            this.isDragging = false;
+            this.selection = true;
+          });
+    }
+
+
     function filterBookingDate(date) {
         let tableIDArray =  []
 
@@ -299,7 +372,7 @@ import TableDatePicker from "../components/datepicker";
                     <TableDatePicker isModal={false} startDate={selectedDate} setStartDate={setSelectedDate} timeSelect={false} onChange={updateMap(selectedDate, canvas)} bookedTables={[]}/>
                     <button style={{all: "unset", cursor: "pointer"}} onClick={() => changeDate("next")}><img src="../next.png" height="40px" width="40px" /></button>
                 </div>
-                <canvas id="canvas"></canvas>
+                <canvas id="canvas" className={styles.canvas}></canvas>
                 <span id="toolTip" className={styles.toolTip}></span>
                 {state.seen ? <Modal userID={session.user.id} userTeamId={session.user.teamId} userEmail={session.user.email} tableID={rectData.tableID} roomID={rectData.roomID} team={rectData.team} tableTeamId={rectData.teamId} bookedTables={bookedTables} bookedRoomTimes={bookedRoomTimes} toggle={togglePop} setBookedTables={setBookedTables} setBookedRoomTimes={setBookedRoomTimes}>
 
